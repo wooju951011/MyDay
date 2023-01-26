db8e741a541c17bca5ef78823538829c2cc5f651
Jiny Kim <kjinyz@naver.com>
2023-01-26 10:50:43 +0900

    Created Project
    Created New Project

---
+++ MyDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDay
+//
+//  Created by Jiny on 2023/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
